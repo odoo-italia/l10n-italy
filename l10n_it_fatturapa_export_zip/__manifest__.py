22e7cefca3a0b8622f06a8d30f76c305629230aa
gborruso@dinamicheaziendali.it <gborruso@dinamicheaziendali.it>
2021-01-29 12:07:44 +0100

    [MIG] l10n_it_fatturapa_export_zip: Migration to 14.0

--- l10n_it_fatturapa_export_zip/__manifest__.py
+++ l10n_it_fatturapa_export_zip/__manifest__.py
@@ -1,12 +1,12 @@
 # Copyright 2019 Sergio Corato (https://efatto.it)
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
-    "name": "Italian Localization - Fattura elettronica - Export ZIP",
+    "name": "ITA - Fattura elettronica - Export ZIP",
     "summary": "Permette di esportare in uno ZIP diversi file XML di "
     "fatture elettroniche",
-    "version": "12.0.1.0.1",
+    "version": "14.0.1.0.0",
     "development_status": "Beta",
-    "category": "other",
+    "category": "Localization/Italy",
     "website": "https://github.com/OCA/l10n-italy",
     "author": "Efatto.it di Sergio Corato, Odoo Community Association (OCA)",
     "maintainers": ["sergiocorato"],
@@ -17,7 +17,11 @@
         "l10n_it_fatturapa_out",
         "l10n_it_fatturapa_in",
     ],
+    "external_dependencies": {
+        "python": ["zipfile"],
+    },
     "data": [
+        "security/ir.model.access.csv",
         "wizard/export_fatturapa_view.xml",
         "views/attachment_view.xml",
     ],
