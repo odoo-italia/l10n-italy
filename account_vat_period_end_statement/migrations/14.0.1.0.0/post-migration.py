1bfdb4f96834c0c7333f4120d09294b1d7400dc1
Alfredo Zamora <alfredo.zamora@agilebg.com>
2021-04-02 13:57:17 +0200

    [MIG] account_vat_period_end_statement: Migration to 14.0

---
+++ account_vat_period_end_statement/migrations/14.0.1.0.0/post-migration.py
@@ -0,0 +1,11 @@
+#  Copyright 2021 Alfredo Zamora - Agile Business Group
+#  License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from openupgradelib import openupgrade  # pylint: disable=W7936
+
+
+@openupgrade.migrate()
+def migrate(env, version):
+    openupgrade.load_data(
+        env.cr, "account_credit_control", "migrations/14.0.1.0.0/noupdate_changes.xml"
+    )
