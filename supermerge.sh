e2de9a00b8d431a8c456f5cc7bdd0d58fb263d5c
Marco Colombo <marco.colombo@gmail.com>
2021-05-07 15:56:05 +0200

    merged #2220 #2230

--- supermerge.sh
+++ supermerge.sh
@@ -58,11 +58,11 @@ git pull --no-ff --no-edit --quiet https://github.com/OCA/l10n-italy pull/2202/h
 git pull --no-ff --no-edit --quiet https://github.com/OCA/l10n-italy pull/2203/head
 git pull --no-ff --no-edit --quiet https://github.com/OCA/l10n-italy pull/2205/head
 git pull --no-ff --no-edit --quiet https://github.com/OCA/l10n-italy pull/2212/head
-git pull --no-ff --no-edit --quiet https://github.com/OCA/l10n-italy pull/2220/head
+# merged # git pull --no-ff --no-edit --quiet https://github.com/OCA/l10n-italy pull/2220/head
 # merged # git pull --no-ff --no-edit --quiet https://github.com/OCA/l10n-italy pull/2225/head
 git pull --no-ff --no-edit --quiet https://github.com/OCA/l10n-italy pull/2228/head
 git pull --no-ff --no-edit --quiet https://github.com/OCA/l10n-italy pull/2229/head
-git pull --no-ff --no-edit --quiet https://github.com/OCA/l10n-italy pull/2230/head
+# merged # git pull --no-ff --no-edit --quiet https://github.com/OCA/l10n-italy pull/2230/head
 # merged # git pull --no-ff --no-edit --quiet https://github.com/OCA/l10n-italy pull/2234/head
 git pull --no-ff --no-edit --quiet https://github.com/OCA/l10n-italy pull/2238/head
 git pull --no-ff --no-edit --quiet https://github.com/OCA/l10n-italy pull/2246/head
